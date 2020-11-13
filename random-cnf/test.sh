1aae1f69385f617cde4468aa7e487c9eade6915f
eichelb4rt <maurice.wenig@gmail.com>
2020-11-13 16:00:46 +0100

    made lingeling a var in test-script

--- random-cnf/test.sh
+++ random-cnf/test.sh
@@ -2,16 +2,17 @@
 # SHEBANG
 
 t=200
-n=4
-c=16
-k=4
+n=5
+c=31
+k=5
 out="out"
+lingeling="./lingeling/lingeling"
 
-./random-cnf.py "$t" "$n" "$c" "$k" -o "$out" &&
+./random-cnf.py "$t" "$n" "$c" "$k" -o "$out" || exit 1
 
 i=0
 while [ "$i" -lt "$t" ]; do
     echo $(basename "./$out/random_cnf_$i.txt") 
-    ./lingeling/lingeling "./$out/random_cnf_$i.txt" | grep SATIS
+    "$lingeling" "./$out/random_cnf_$i.txt" | grep SATIS
     ((++i))
 done
