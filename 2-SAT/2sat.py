2d37ae24706ac59965b56a4fc132104efa6d2428
eichelb4rt <maurice.wenig@gmail.com>
2020-11-24 18:00:52 +0100

    added 2-sat

---
+++ 2-SAT/2sat.py
@@ -0,0 +1,193 @@
+import read_dimacs as dimacs
+#!/bin/python3
+# SHEBANG
+
+import os
+import sys
+import random
+import argparse
+import numpy as np
+from typing import List, Tuple
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        metavar = 'input',
+        dest = 'input',
+        type = str,
+        help = 'Input file where DIMACS notation of a formular is stored.'
+    )
+    args = parser.parse_args()
+    with open(args.input, "r") as f:
+        formula = dimacs.read_cnf(f.readlines())
+        print(two_sat(formula))
+
+def two_sat(f: List[List[int]]) -> (bool, List[Tuple[int, bool]]):
+    """Applies the 2-SAT algorithm to the given formula f.
+
+    Parameters
+    ----------
+    f : List[List[int]]
+        The given formula.
+
+    Returns
+    -------
+    bool, List[Tuple[int, bool]]
+        True plus fulfilling assignment if f is satisfiable, False and None if not.
+    """
+
+    # preprocessing
+    if empty_set_contained(f):
+        return False
+    # loop
+    assignments = []
+    while var := get_var(f):  # while vars(f) != empty set
+        # assign 0 to the var
+        new_f, new_assignments = unit_propagation(f, (var, False))
+        if empty_set_contained(new_f):  # empty set is contained
+            # assign 1 to the var
+            new_f, new_assignments = unit_propagation(f, (var, False))
+            if empty_set_contained(new_f):
+                return False, None
+        f = new_f
+        for assignment in new_assignments:
+            assignments.append(assignment)
+    # vars(f) == empty set
+    return True, assignments
+
+def get_var(f: List[List[int]]) -> int:
+    """Gets a variable in the formula f, preferably a variable in a unit clause.
+
+    Parameters
+    ----------
+    f : List[List[int]]
+        The given forumula.
+
+    Returns
+    -------
+    int
+        Variable in formula, preferably a variable in a unit clause, None if there is no variable left
+    """
+
+    if literal := find_unit_clause(f):
+        return abs(literal)
+    
+    # return first variable
+    if len(f) != 0:
+        if len(f[0]) != 0:
+            return f[0][0]
+    
+    return None
+
+def empty_set_contained(f: List[List[int]]) -> bool:
+    """Checks if empty set is contained in a given formula.
+
+    Parameters
+    ----------
+    f : List[List[int]]
+        The given formula.
+
+    Returns
+    -------
+    bool
+        True if empty set is contained, False if not.
+    """
+
+    for clause in f:
+        if len(clause) == 0:
+            return True
+    return False
+
+def unit_propagation(f: List[List[int]], assignment: Tuple[int, bool]) -> (List[List[int]], List[Tuple[int, bool]]):
+    """Applies the assignment plus unit propagation to the given formula.
+
+    Parameters
+    ----------
+    f : List[List[int]]
+        The given formula.
+    assignment : Tuple[int, bool]
+        A variable and the assigned boolean.
+
+    Returns
+    -------
+    (List[List[int]], List[Tuple[int, bool])
+        The given formula with the assignment assigned plus unit propagation and the assignment itself.
+    """
+
+    # apply the given assignment
+    assignments = [assignment]
+    f = apply_assignment(f, assignment)
+
+    # propagate
+    while literal := find_unit_clause(f): # while unit clauses are found
+        if literal > 0:
+            assigned_value = True
+        else:
+            assigned_value = False
+        # apply the propagated assignment
+        assignment = (abs(literal), assigned_value)
+        assignments.append(assignment)
+        f = apply_assignment(f, assignment)
+    return f, assignments
+
+def find_unit_clause(f: List[List[int]]) -> int:
+    """Finds a unit clause in the given formula f.
+
+    Parameters
+    ----------
+    f : List[List[int]]
+        The given formula.
+
+    Returns
+    -------
+    int
+        Literal if found in unit clause. 0 if no unit clause was found.
+    """
+
+    for clause in f:
+        if len(clause) == 1:
+            return clause[0]
+    return None
+
+def apply_assignment(f: List[List[int]], assignment: Tuple[int, bool]) -> List[List[int]]:
+    """Applies the given assignment to the given formula f.
+
+    Parameters
+    ----------
+    f : List[List[int]]
+        The given formula.
+    assignment : Tuple[int, bool]
+        A variable and the assigned boolean.
+
+    Returns
+    -------
+    List[List[int]]
+        The given formula where the assignment is applied.
+    """
+
+    # now to define what literals remove a clause from the formula or the literal from the clause if contained
+    var = assignment[0]
+    assigned_value = assignment[1]
+    removes_literal_from_clause = -1 * var if assigned_value == True else var   # == True just for readability, ik i don't need it
+    removes_clause_from_formula = -1 * var if assigned_value == False else var    # same here
+
+    # now remove the things
+    edited_formula = True   # to start the loop
+    # we're done if we haven't edited any clauses or removed any from the formula
+    while edited_formula:
+        edited_formula = False  # never set to True if nothing is edited
+        for i, clause in enumerate(f):
+            if removes_literal_from_clause in clause:
+                # remove the literal
+                f[i].remove(removes_literal_from_clause)
+                edited_formula = True
+                break
+            elif removes_clause_from_formula in clause: # elif because the cases are mutually exclusive
+                # remove the clause
+                f.remove(clause)
+                edited_formula = True
+                break
+    return f
+
+if __name__ == "__main__":
+    main()
---
+++ 2-SAT/read_dimacs.py
@@ -0,0 +1,96 @@
+#!/bin/python3
+# SHEBANG
+
+import argparse
+from typing import List
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        metavar = 'input',
+        dest = 'input',
+        type = str,
+        help = 'Input file where DIMACS notation of a formular is stored.'
+    )
+    args = parser.parse_args()
+    with open(args.input, "r") as f:
+        print(read_cnf(f.readlines()))
+
+def get_variables_in_dimacs(dimacs: List[str]) -> int:
+    """Get the number of variables that are set in the DIMACS encoding.
+
+    Parameters
+    ----------
+    dimacs : List[str]
+        DIMACS encoded CNF. Pass it with file.readlines()
+
+    Returns
+    -------
+    int
+        Number of variables.
+    """
+    
+    # determine where the problem starts
+    start_index = 0
+    for line_number, line in enumerate(dimacs):
+        if line.startswith("p cnf"):
+            start_index = line_number
+            break
+    
+    # get n and c.
+    try:
+        problem_args = dimacs[start_index].replace('\n', '').split(' ')  # delete the line break and split it into args
+        return int(problem_args[2])
+        
+    # if something goes wrong, it was probably not DIMACS encoded.
+    except Exception:
+        raise SyntaxError("This is not a DIMACS encoded formula.")
+
+def read_cnf(dimacs: List[str]) -> List[List[int]]:
+    """Reads a DIMACS encoded CNF.
+
+    Parameters
+    ----------
+    dimacs : List[str]
+        DIMACS encoded CNF. Pass it with file.readlines().
+
+    Returns
+    -------
+    List[List[int]]
+        The CNF.
+    """
+
+    # determine where the problem starts
+    start_index = 0
+    for line_number, line in enumerate(dimacs):
+        if line.startswith("p cnf"):
+            start_index = line_number
+            break
+    
+    # get n and c.
+    try:
+        problem_args = dimacs[start_index].replace('\n', '').split(' ')  # delete the line break and split it into args
+        n = int(problem_args[2])
+        c = int(problem_args[3])
+        
+    # if something goes wrong, it was probably not DIMACS encoded.
+    except Exception:
+        raise SyntaxError("This is not a DIMACS encoded formula.")
+    
+    # check if it's 2-SAT
+    if c != 2:
+        raise SyntaxError("This is not a 2-SAT.")
+    
+    # build the cnf
+    cnf = []
+    for line_number, clause_dimacs in enumerate(dimacs[start_index+1:]):   # clauses come after the first line
+        clause_dimacs = clause_dimacs.replace('\n', '') # delete line break
+        clause = [int(literal) for literal in clause_dimacs.split(" ")] # get the literals seperated by space
+        if clause[-1] == 0: # 0-terminated clauses
+            cnf.append(clause[:-1]) # append the list without the terminating 0
+        else:
+            raise SyntaxError(f"No 0-terminated clause in line {line_number}")
+    return cnf
+
+if __name__ == "__main__":
+    main()
