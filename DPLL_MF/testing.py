dbe4a2a1f1581185f708756c840e9d4b56d6ea14
eichelb4rt <maurice.wenig@gmail.com>
2021-03-15 22:18:14 +0100

    removed random cnfs where they dont belong and testing.py everywhere

--- DPLL_MF/testing.py
+++
@@ -1,7 +0,0 @@
-myvar = 0
-
-def main(abc: int = globals()["myvar"]):
-    print(abc)
-
-if __name__ == "__main__":
-    main(2)
--- comparisons/test.sh
+++ comparisons/test.sh
@@ -3,7 +3,7 @@
 
 t=50
 n=5
-c=15
+c=25
 k=3
 random_cnf_dir="../random-cnf"
 random_cnf_tool="$random_cnf_dir/random-cnf.py"
@@ -13,16 +13,33 @@ two_sat="../2-SAT/two_sat.py"
 dpll="../DPLL/dpll.py"
 dpll_mf="../DPLL_MF/dpll_mf.py"
 cdcl="../CDCL/cdcl.py"
-solver="$cdcl"
 
 "$random_cnf_tool" "$t" "$n" "$c" "$k" -o "$out" || exit 1
 
+solver="$dpll"
 i=0
+satisfiable_count=0
+unsatisfiable_count=0
+mistakes=0
 while [ "$i" -lt "$t" ]; do
     file="./$out/random_cnf_$i.txt"
     echo $(basename "$file")
-    "$lingeling" "$file" | grep SATISFIABLE
-    "$solver" "$file"
+    # get the results for lingeling and our solver
+    result_lingeling=$("$lingeling" "$file" | grep SATISFIABLE)
+    result_solver=$("$solver" "$file")
+    # make them fit each others format
+    if [ "$result_lingeling" == "s SATISFIABLE" ]; then
+        result_lingeling="Satisfiable"
+        ((++satisfiable_count))
+    else
+        result_lingeling="Unsatisfiable"
+        ((++unsatisfiable_count))
+    fi
+    if [ "$result_lingeling" != "$result_solver" ]; then ((++mistakes)); fi
+    # print them both
+    echo "$result_lingeling"
+    echo "$result_solver"
     ((++i))
     echo -e "\n"
 done
+echo -e "Satisfiable: $satisfiable_count\nUnsatisfiable: $unsatisfiable_count\nMistakes: $mistakes"
